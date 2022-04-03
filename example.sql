76932e9a797f241c859bc64fd05e7ff7e119e644
Elena <pashtanova.spb@gmail.com>
2022-04-03 18:23:50 +0300

    2

---
+++ example.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE example;
+
+USE example;
+
+CREATE TABLE example_tbl(
+
+id INT,
+name VARCHAR(100)
+);
+
+INSERT INTO example_tbl VALUES (1, 'example');
+
+SELECT * FROM example_tbl;
+
+INSERT INTO example_tbl VALUES (2, 'example');
